a78337dc5b89ec9e18a7d1fd7c473deb5a4d19ed
Douglas Duarte <douglasduarte.eng@gmail.com>
2021-09-02 15:10:09 -0300

    :package: Sticky navigation with the intersection observer API

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 //========== Modal window ================
 const openModal = function (e) {
@@ -106,3 +107,22 @@ const handleHover = function (e) {
 // Passsing 'argument' into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//================ Sticky navigation ====================
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  nav.classList.add('sticky');
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
